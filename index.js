bbb61fd7e4a74fa05a9dc0c3c4fef4d5d136f782
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2021-03-09 21:45:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -107,16 +107,12 @@ generateApple()
 
 function control(e) {
     if (e.keyCode === 39) {
-        console.log('right pressed')
         direction = 1
     } else if (e.keyCode === 38) {
-        console.log('up pressed')
         direction = -width
     } else if (e.keyCode === 37) {
-        console.log('left pressed')
         direction = -1
     } else if (e.keyCode === 40) {
-        console.log('down pressed')
         direction = +width
     }
 }
